f8dbdeb6963e56d1440e7ca79f3765e3af751e54
BurakKurt <krtburak@outlook.com>
2023-03-07 18:31:51 +0300

    step 1 object components added

---
+++ components/_objectsCreated.js
@@ -0,0 +1,28 @@
+import { Questions } from '../data.js';
+import QuestionClass from './_questionObject.js';
+
+export const questionObjects = [];
+
+export default function ObjectsCreated(
+  dataName = String,
+  questionNumbers = Number,
+  justQuestion = Boolean
+) {
+  for (let i = 0; i < questionNumbers; i++) {
+    let questionRandom = Math.floor(Math.random() * Questions[dataName].length);
+    Questions[dataName].map(question => {
+      if (question.id === questionRandom) {
+        questionObjects.push(
+          new QuestionClass(
+            question.id,
+            question.question,
+            question.answer,
+            question.questionTurkish,
+            question.answerTurkish,
+            justQuestion // just question or question and answer
+          )
+        );
+      }
+    });
+  }
+}
---
+++ components/main.js
@@ -0,0 +1,5 @@
+import ObjectsCreated, { questionObjects } from './_objectsCreated.js';
+
+ObjectsCreated('numbers-alphabet', 4, true);
+
+console.log(questionObjects);
