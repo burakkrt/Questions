22cf71741900facd6782aa2c8a9c1d3b957c1ac6
BurakKurt <krtburak@outlook.com>
2023-03-09 14:55:14 +0300

    Finally, the temporary arrangements were recorded.

--- components/main.js
+++ components/main.js
@@ -3,21 +3,39 @@ import QuestionObject from './questionObject.js';
 import Message from './message.js';
 import CreateCheck from './createCheck.js';
 import questionDOM from './questionDOM.js';
+import questionResultCheck from './questionResultCheck.js';
 
 // Show functions to window
 window.createdQuestions = createdQuestions;
+window.resultCheck = resultCheck;
 
 // Menu Item Created
 const menuElement = document.querySelector('#menuContent');
 MenuItems(menuElement);
 
 // Created Question and Export to DOM
-const contentElement = document.querySelector('#questionContent');
+const contentElement = document.getElementById('questionContent');
 function createdQuestions(questionTitle) {
     const checkResult = CreateCheck(questionTitle);
     if (checkResult.check) {
-        QuestionObject(questionTitle, checkResult.questionNumber, checkResult.type);
+        QuestionObject(questionTitle, checkResult.questionNumber, checkResult.type, contentElement);
     }
+}
+
+// Questios Local Stroage Export Dom Content
+questionDOM(JSON.parse(localStorage.getItem('questions')), contentElement);
+
+function resultCheck(objectId) {
+    let objectIs;
+    for (let object of JSON.parse(localStorage.getItem('questions'))) {
+        if (objectId === object.objectId) {
+            objectIs = object;
+            break;
+        }
+    }
+
+    const questionResult = questionResultCheck(objectIs, document.getElementById(`input${objectId}`).value);
+    console.log(questionResult);
 }
 
 document.querySelector('#burgerMenu').addEventListener('click', () => {
@@ -47,3 +65,40 @@ document.querySelector('#burgerMenu').addEventListener('click', () => {
         burgerMenu.parentElement.classList.add('pr-5');
     }
 });
+
+// <!-- col -->
+// <div class="rounded-md bg-slate-300">
+//     <div class="flex h-full flex-col">
+//         <div
+//             class="flex flex-row items-center justify-between rounded-t-md bg-zinc-700 p-2 text-stone-200"
+//         >
+//             <div>
+//                 <p>Question No</p>
+//             </div>
+//             <div>
+//                 <button class="mr-2">
+//                     <i class="fa-sharp fa-solid fa-bolt mr-2"></i>Cevabı Göster
+//                 </button>
+//                 <button><i class="fa-solid fa-circle-xmark mr-2"></i>Sil</button>
+//             </div>
+//         </div>
+//         <div class="flex-auto p-2">
+//             <div class="flex h-full flex-col justify-between gap-y-2">
+//                 <div>
+//                     <span>Question :</span>
+//                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, et.</p>
+//                     <div class="">
+//                         <p class="">Lorem ipsum dolor sit amet consectetur.</p>
+//                     </div>
+//                 </div>
+//                 <div>
+//                     <span>Answer :</span>
+//                     <input class="block w-full px-1" type="text" />
+//                     <button class="mt-2 rounded-md bg-slate-400 px-4 py-1.5" onclick="">
+//                         Test
+//                     </button>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+// </div>
