6fab057c098ed6f25cf75c0e8475f87955b745af
BurakKurt <krtburak@outlook.com>
2023-03-09 17:28:58 +0300

    add style class and correct answer since answer is wrong.

--- components/questionResultCheck.js
+++ components/questionResultCheck.js
@@ -14,6 +14,16 @@ export default function questionResultCheck(thisObject, userValue) {
         return true;
     } else {
         setLocalStroage(thisObject, false, userValue);
+        document.getElementById(thisObject.objectId).classList.add('false'); //Css .question-box.false class
+        let element = `
+            <div class="mt-1 bg-red-900 p-2">
+                <span class="font-semibold text-green-400">Correct answer :</span>
+                <p class="inline">
+                    2Selam Kaira. Ben Burak. Tanıştığımıza memnun oldum. (Pilisdt tu mit yu)
+                </p>
+            </div>
+        `;
+        document.getElementById(`resultDiv${thisObject.objectId}`).insertAdjacentHTML('beforeend', element);
         return false;
     }
 }
