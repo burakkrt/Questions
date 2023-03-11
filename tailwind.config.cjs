7ab8d60c70b886e8a48c17a1b8d35d87d4fa03bb
BurakKurt <krtburak@outlook.com>
2023-03-11 12:55:41 +0300

    Style classes in javascript files are included in tailwind css.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./**/*.{html,js}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
